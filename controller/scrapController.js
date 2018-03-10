66a119c9b60b7aab58e9408d0de38566a4d38f69
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-03-10 19:45:20 +0530

    updated scrap controller

--- controller/scrapController.js
+++ controller/scrapController.js
@@ -1,9 +1,10 @@
-const request = require("request");
+const request = require("request-promise");
 const mongoose = require("mongoose");
 const cheerio = require("cheerio");
 
 module.exports = {
-  scrapIt
+  scrapIt,
+  processService
 };
 
 function initialize() {
@@ -17,20 +18,17 @@ function initialize() {
       }
     };
 
-    request(options, function(error, response, html) {
-      if (!error) {
-        let processedJson = JSON.parse(html);
-        return resolve(processedJson.meta["X-Total-Page"]);
-      } else {
-        return reject(error);
-      }
+    request(options).then(html => {
+      let processedJson = JSON.parse(html);
+      return resolve(processedJson.meta["X-Total-Page"]);
     });
   });
 }
 
-function scrapIt(axNo) {
+function scrapIt() {
+  let baseUrl = "https://www.carworkz.com";
   return new Promise((resolve, reject) => {
-    return initialize
+    return initialize()
       .then(maxNo => {
         let theArr = [];
         for (let j = 0; j < maxNo; j++) {
@@ -46,8 +44,8 @@ function scrapIt(axNo) {
             }
           };
 
-          request(options, function(error, response, html) {
-            if (!error) {
+          request(options)
+            .then(html => {
               let processedJson = JSON.parse(html);
               let processedHtml = processedJson.listing;
               let lol = unescape(processedHtml);
@@ -75,19 +73,64 @@ function scrapIt(axNo) {
                   (payloadObject.mobile_no = $(article)
                     .find(".mobile_no")
                     .text());
+
+                payloadObject.service_url =
+                  baseUrl +
+                  $(article)
+                    .find(".select-jobs")
+                    .attr("href");
+
                 theArr.push(payloadObject);
                 if (processedJson.meta["X-Total-Count"] == theArr.length) {
                   return resolve(theArr);
                 }
               });
-            } else {
+              //   return resolve(theArr);
+            })
+            .catch(error => {
               return reject(error);
-            }
-          });
+            });
         }
       })
       .catch(error => {
         return reject(error);
       });
+  });
+}
+
+function processService(payload) {
+  return new Promise((resolve, reject) => {
+    let data = [];
+    payload.forEach(
+      element => {
+        let options = {
+          url: element.service_url,
+          transform: function(body) {
+            return cheerio.load(body);
+          }
+        };
+
+        request(options)
+          .then($ => {
+            let services_single = [];
+            $(".serv_custom_list").each((i, services) => {
+              services_single.push({
+                services: $(services)
+                  .find("li")
+                  .text()
+              });
+            });
+            element.services = services_single;
+            data.push(element);
+            console.log(data.length);
+          })
+          .catch(error => {
+            // continue;
+          });
+      },
+      () => {
+        return resolve(data);
+      }
+    );
   });
 }
