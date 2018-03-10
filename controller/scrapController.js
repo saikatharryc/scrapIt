f60a2044d4d33112a1eb8f768dd90032d28d2888
saikat chakrabortty <saikatchakrabortty2@gmail.com>
2018-03-10 21:22:10 +0530

    search  & update function added

--- controller/scrapController.js
+++ controller/scrapController.js
@@ -7,7 +7,9 @@ const Scrapy = mongoose.model("Scrapy");
 module.exports = {
   scrapIt,
   processService,
-  saveIt
+  saveIt,
+  update,
+  search
 };
 
 function initialize() {
@@ -62,37 +64,51 @@ function scrapIt() {
                   .text()),
                   (payloadObject.rating = $(article)
                     .find(".number_rating")
-                    .text()),
+                    .text()
+                    ? parseFloat(
+                        $(article)
+                          .find(".number_rating")
+                          .text()
+                      )
+                    : 0),
                   (payloadObject.likes = $(article)
                     .find(".per_votes")
-                    .text()),
+                    .text()
+                    ? parseInt(
+                        $(article)
+                          .find(".per_votes")
+                          .text()
+                      )
+                    : 0),
                   (payloadObject.votes = $(article)
                     .find(".txt_votes")
                     .text()
-                    .split(" ")[0]),
+                    .split(" ")[0]
+                    ? parseInt(
+                        $(article)
+                          .find(".txt_votes")
+                          .text()
+                          .split(" ")[0]
+                      )
+                    : 0),
                   (payloadObject.location = $(article)
                     .find(".location_distance")
                     .text()),
                   (payloadObject.mobile_no = $(article)
                     .find(".mobile_no")
-                    .text());
+                    .text()
+                    ? parseInt(
+                        $(article)
+                          .find(".mobile_no")
+                          .text()
+                      )
+                    : null);
 
                 payloadObject.service_url =
                   baseUrl +
                   $(article)
                     .find(".select-jobs")
                     .attr("href");
-                //tackle in some other way if possible
-                // processService(payloadObject)
-                //   .then(data => {
-                //     theArr.push(data);
-                //     if (processedJson.meta["X-Total-Count"] == theArr.length) {
-                //       return resolve(theArr);
-                //     }
-                //   })
-                //   .catch(error => {
-                //     return reject(error);
-                //   });
 
                 theArr.push(payloadObject);
                 if (processedJson.meta["X-Total-Count"] == theArr.length) {
@@ -125,11 +141,11 @@ function processService(element) {
       .then($ => {
         let services_single = [];
         $(".serv_custom_list").each((i, services) => {
-          services_single.push({
-            services: $(services)
+          services_single.push(
+            $(services)
               .find("li")
               .text()
-          });
+          );
         });
         element.services = services_single;
         return resolve(element);
@@ -142,4 +158,59 @@ function processService(element) {
 
 function saveIt(payload) {
   return Scrapy.insertMany(payload);
+}
+
+function update() {
+  return new Promise((resolve, reject) => {
+    let some_data = [];
+    return Scrapy.find({})
+      .exec()
+      .then(data => {
+        let i = 0;
+        for (let elements of data) {
+          i++;
+          return processService(elements)
+            .then(updated_data => {
+              elements.services = updated_data.services;
+              some_data.push(elements);
+              elements.save();
+            })
+            .catch(error => {
+              return reject(error);
+            });
+          if (data.length == i) {
+            return resolve(some_data);
+          }
+        }
+      });
+  });
+}
+
+function search(payload) {
+  let query = {};
+  if (payload.name) {
+    Object.assign(query, { name: { $regex: payload.name, $options: "i" } });
+  }
+  if (payload.rating_gt) {
+    Object.assign(query, { rating: { $gt: Number(payload.rating_gt) } });
+  }
+  if (payload.rating_lt) {
+    Object.assign(query, { rating: { $lt: Number(payload.rating_lt) } });
+  }
+  if (payload.location) {
+    Object.assign(query, {
+      location: { $regex: payload.location, $options: "i" }
+    });
+  }
+  if (payload.location) {
+    Object.assign(query, {
+      mobile_no: { $regex: Number(payload.mobile_no), $options: "i" }
+    });
+  }
+  if (payload.services) {
+    Object.assign(query, { services: { $regex: services, $options: "i" } });
+  }
+  return Scrapy.find(query)
+    .lean()
+    .exec();
 }
